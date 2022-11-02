b83d5da2b817d0a944a1d81ee34daf88a7da2131
Himanshu <gautamhimanshu689@gmail.com>
2022-11-02 17:56:07 +0530

    add api path

--- src/api/index.js
+++ src/api/index.js
@@ -1,60 +1,60 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts'
+const url = `https://immense-river-04196.herokuapp.com/posts`
 
 export const fetchPost = () =>
 
     axios.get(url)
 
-export const createPost = (newPost) => axios.post('http://localhost:5000/posts/api/user', newPost, {
+export const createPost = (newPost) => axios.post('https://immense-river-04196.herokuapp.com/posts/api/user', newPost, {
     headers: {
         'auth-token': JSON.parse(localStorage.getItem('auth-token')),
     }
 });
 
-export const register = (userData) => axios.post('http://localhost:5000/posts/api/register', userData);
+export const register = (userData) => axios.post('https://immense-river-04196.herokuapp.com/posts/api/register', userData);
 
-export const login = (userLogin) => axios.post('http://localhost:5000/posts/api/login', userLogin);
+export const login = (userLogin) => axios.post('https://immense-river-04196.herokuapp.com/posts/api/login', userLogin);
 
-export const data = () => axios.get('http://localhost:5000/posts/api/getUser')
+export const data = () => axios.get('https://immense-river-04196.herokuapp.com/posts/api/getUser')
 
-export const like = (likeData) => axios.patch(`http://localhost:5000/posts/api/like/${likeData.id}`, likeData, {
+export const like = (likeData) => axios.patch(`https://immense-river-04196.herokuapp.com/posts/api/like/${likeData.id}`, likeData, {
     headers: {
         'auth-token': JSON.parse(localStorage.getItem('auth-token')),
     }
 })
 
-export const updateUser = (userdata) => axios.put(`http://localhost:5000/posts/api/updateUser/${userdata.id}`, userdata, {
+export const updateUser = (userdata) => axios.put(`https://immense-river-04196.herokuapp.com/posts/api/updateUser/${userdata.id}`, userdata, {
     headers: {
         'auth-token': JSON.parse(localStorage.getItem('auth-token')),
     }
 })
 
-export const userLoginData = (id) => axios.get(`http://localhost:5000/posts/api/userdata/${id}`, {
+export const userLoginData = (id) => axios.get(`https://immense-river-04196.herokuapp.com/posts/api/userdata/${id}`, {
     headers: {
         'auth-token': JSON.parse(localStorage.getItem('auth-token')),
     }
 })
 
-export const profileImage = (profile, id) => axios.patch(`http://localhost:5000/posts/api/profileUpdate/${id}`, profile, {
+export const profileImage = (profile, id) => axios.patch(`https://immense-river-04196.herokuapp.com/posts/api/profileUpdate/${id}`, profile, {
     headers: {
         'auth-token': JSON.parse(localStorage.getItem('auth-token')),
         'Content-Type': `multipart/form-data`,
     }
 })
 
-export const postcomment = (comment, id, userid) => axios.patch(`http://localhost:5000/posts/api/comment/${id}`, comment, {
+export const postcomment = (comment, id, userid) => axios.patch(`https://immense-river-04196.herokuapp.com/posts/api/comment/${id}`, comment, {
     headers: {
         'auth-token': JSON.parse(localStorage.getItem('auth-token')),
     }
 })
 
-export const fetchComment = (id) => axios.get(`http://localhost:5000/posts/api/fetchComment/${id}`, {
+export const fetchComment = (id) => axios.get(`https://immense-river-04196.herokuapp.com/posts/api/fetchComment/${id}`, {
     headers: {
         'auth-token': JSON.parse(localStorage.getItem('auth-token')),
     }
 })
-export const deletePost = (id) => axios.delete(`http://localhost:5000/posts/api/delete/${id}`, {
+export const deletePost = (id) => axios.delete(`https://immense-river-04196.herokuapp.com/posts/api/delete/${id}`, {
     headers: {
         'auth-token': JSON.parse(localStorage.getItem('auth-token')),
     }
